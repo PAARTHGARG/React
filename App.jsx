4e3f9a00056ba24ac53d4b6edaa02b3f2ff3f532
PAARTHGARG <90719768+PAARTHGARG@users.noreply.github.com>
2025-04-18 00:15:44 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+
+import Paarth from "./paarth" 
+function App() {
+  const username="Paarth Coding"
+
+  return (
+    <>
+    <Paarth />
+    <h1>React learning with vite {username}</h1> //evaluated syntax
+    <p>Test para</p>
+    </>
+  )
+}
+
+export default App
